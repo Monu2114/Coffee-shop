c2920c94605e681a74c8fe57d49acc144978e026
Monu_2114 <monu.nanabala@gmail.com>
2025-01-25 19:42:33 +0530

    half of instant coffee

--- app/_components/InstantCofee.jsx
+++ app/_components/InstantCofee.jsx
@@ -4,16 +4,18 @@ import React from "react";
 const InstantCofee = () => {
   return (
     <div className="bg-gray-50">
-      <div className="conatiner">
+      <div className="conatiner ">
         <div className="grid grid-cols-2">
-          <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-bold ">Instant Coffee At Your Home</h1>
-            <p>
+          <div className=" ml-72 flex flex-col gap-8 mt-48">
+            <h1 className="text-5xl font-bold w-96">
+              Instant Coffee At Your Home
+            </h1>
+            <p className="text-gray-500">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
               reprehenderit repellat aut molestias est, odit quos voluptatem
               sint dicta, mollitia minima corporis accusantium optio vero?
             </p>
-            <Button className="py-3 px-4 w-44 rounded-md shadow-lg hover:scale-105 transition-all text-lg font-normal tracking-wide bg-black text-white">
+            <Button className="py-3 px-4 w-52 rounded-md shadow-lg hover:scale-105 transition-all text-lg font-normal tracking-wide bg-black text-white">
               Download Your App
             </Button>
           </div>
